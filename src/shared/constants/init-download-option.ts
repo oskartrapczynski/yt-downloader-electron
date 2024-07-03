1476a2846c2c128d2d2c2a9edcd1764f483f97c5
oskartrapczynski <oskardenis167@gmail.com>
2024-07-03 21:14:32 +0200

    fix init download option

--- src/shared/constants/init-download-option.ts
+++ src/shared/constants/init-download-option.ts
@@ -2,5 +2,5 @@ import { DOWNLOAD_OPTION } from './download-option'
 
 export const INIT_DOWNLOAD_OPTION = Object.assign(
   {},
-  ...Object.keys(DOWNLOAD_OPTION).map((key) => ({ [key]: undefined }))
+  ...Object.values(DOWNLOAD_OPTION).map((value) => ({ [value]: undefined }))
 )
